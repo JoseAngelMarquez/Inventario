140a28644626c6be99636549eec6835050e93761
JoseAngelMarquez <joseangelmarquezespina060503@gmail.com>
2025-07-28 22:05:12 -0600

    Crea schema de usuarios en models

---
+++ src/models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password
+
+})
